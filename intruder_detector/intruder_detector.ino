acc815ed1adbce9feec7e8b42ebd79fc8b2395e4
Lucas Diaz Iwamoto <between630pm@gmail.com>
2026-04-12 11:52:56 -0400

    added code protection and rgb led

--- intruder_detector/intruder_detector.ino
+++ intruder_detector/intruder_detector.ino
@@ -10,6 +10,10 @@
 #define TRIG_PIN 5
 #define ECHO_PIN 7
 
+#define BLUE 11
+#define GREEN 10
+#define RED 9
+
 long a;
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
@@ -17,9 +21,30 @@ int melody[] = {
   NOTE_C4, NOTE_C7};
 int duration = 50;
 
+/*
+
+0 = 0x16
+1 = 0xC
+2 = 0x18
+3 = 0x5E
+4 = 0x8
+5 = 0x1C
+6 = 0x5A
+7 = 0x42
+8 = 0x52
+9 = 0x4A
+
+*/
+
+int passcode[] = {
+    0x16, 0xC, 0x18, 0x5E
+};
+
 bool armed = true;
 
 bool detected = false;
+
+bool coding = false;
 
 int buzzer = 8;
 
@@ -27,6 +52,7 @@ void setup() {
     Serial.begin(115200);
 
     pinMode(buzzer,OUTPUT);
+    setled(2);
 
     #if defined(__AVR_ATmega32U4__) || defined(SERIAL_PORT_USBVIRTUAL) || defined(SERIAL_USB) /*stm32duino*/|| defined(USBCON) /*STM32_stm32*/ \
         || defined(SERIALUSB_PID)  || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_attiny3217)
@@ -55,19 +81,65 @@ void setup() {
     #endif
 }
 
+void setled(int colour){
+    digitalWrite(RED, 0);
+    digitalWrite(GREEN, 0);
+    digitalWrite(BLUE, 0);
+    switch (colour){
+        case 0:
+            digitalWrite(RED, 255);
+            break;
+        case 1:
+            digitalWrite(GREEN, 255);
+            break;
+        case 2:
+            digitalWrite(BLUE, 255);
+    }
+}
+
 void loop() {
     if (TinyReceiverDecode()) {
         if (TinyIRReceiverData.Command == 0x45){
             armed = true;
+            setled(1);
             digitalWrite(13,HIGH);
             Serial.println("power button received");        
         }
         else if (TinyIRReceiverData.Command == 0x47){
-            armed = false;
-            detected = false;
-            digitalWrite(buzzer,LOW);
-            digitalWrite(13,LOW);
             Serial.println("stop button recieved");
+            if (detected){
+                coding = true;
+                setled(2);
+                int digit = 0;
+                while (coding){
+                    if (TinyReceiverDecode() && !(TinyIRReceiverData.Flags == IRDATA_FLAGS_IS_REPEAT)){
+                        if (TinyIRReceiverData.Command == passcode[digit]){
+                            digit += 1;
+                            setled(1);
+                        }
+                        else {
+                            digit = 0;
+                            setled(0);
+                        }
+                        if (digit == 4){
+                            detected = false;
+                            coding = false;
+                            setled(1);
+                            armed = true;
+                            digitalWrite(buzzer,LOW);
+                            digitalWrite(13,LOW);
+                        }
+                        else {
+                            delay(500);
+                            setled(2);
+                        }    
+                    }
+                }
+            }
+            else {
+                armed = false;
+                setled(0);
+            }
         }
         Serial.println(TinyIRReceiverData.Command, HEX);
     }
